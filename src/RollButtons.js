6b4f044de70bacf96b720cf964ff28adb27c3ae2
erlendve <erlendve@hotmail.com>
2017-02-03 12:59:15 +0100

    updated wrong number for Rollbuttons

--- src/RollButtons.js
+++ src/RollButtons.js
@@ -4,12 +4,13 @@ import Roll from './Roll';
 const RollButtons = () =>  {
   return (<div className="rolls">
     <Roll number={0} />
-    <Roll number={2} />
     <Roll number={1} />
+    <Roll number={2} />
     <Roll number={3} />
     <Roll number={4} />
     <Roll number={5} />
     <Roll number={6} />
+    <Roll number={7} />
     <Roll number={8} />
     <Roll number={9} />
     <Roll number={10} />
